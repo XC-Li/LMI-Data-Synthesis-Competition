f16c543b1668ff7b1e759e066991d76cda2b4e15
Mandy Zhang <mandy.zhang512@gmail.com>
2018-11-06 00:33:20 -0500

    add residual plots for xgboost

--- Pipeline--predicting_true_data.py
+++ Pipeline--predicting_true_data.py
@@ -50,8 +50,8 @@ def reading_data(file_name):
     else:
         df = pd.read_excel(file_name,thousands = ",") #seperations in thousands
     # remove features:
-    categotical_features = ["e_type", "benefit", "Time", "p_grade"]
-    not_include_features = ["MOU", "MOU Title", "Title", "Department", "Record Number", "POBP"]
+    not_include_features = ["MOU", "MOU Title", "Title", "Department", "Record Number", "POBP",
+                            "p_grade","benefit","e_type"]
     selected_features = [i for i in df.columns if i not in not_include_features]
     X_selected = df.loc[:,selected_features]
     # data imputation:
@@ -59,30 +59,7 @@ def reading_data(file_name):
     X_selected["Lump Sum Pay"] = X_selected["Lump Sum Pay"].fillna(0)
     X_selected["Rate"] = X_selected["Rate"].fillna(X_selected["Rate"].mean())
     X_selected["o_pay"] = X_selected["o_pay"].fillna(X_selected["o_pay"].median())
-    X_selected["p_grade"] = X_selected["p_grade"].fillna(-1)
-    X_selected["benefit"] = X_selected["benefit"].fillna(-1)
     final_df = X_selected
-    
-    # change the format of three features into integer: p_grade, benefit, e_type:
-    p_grade_map={}
-    j=0
-    for i in pd.unique(final_df['p_grade']):
-        p_grade_map[i]=j
-        j=j+1
-    benefit_map={}
-    j=0
-    for i in pd.unique(final_df['benefit']):
-        benefit_map[i]=j
-        j=j+1
-    e_type_map={}
-    j=0
-    for i in pd.unique(final_df['e_type']):
-        e_type_map[i]=j
-        j=j+1
-
-    final_df['p_grade']=final_df['p_grade'].map(p_grade_map)
-    final_df['benefit']=final_df['benefit'].map(benefit_map)
-    final_df['e_type']=final_df['e_type'].map(e_type_map)
     return final_df
 
 
@@ -96,21 +73,49 @@ print("========================================================")
 
 def h2o_pred(test):
     h2oTest = h2o.H2OFrame(test)
-    saved_model = h2o.load_model(os.getcwd()+"/XGBoost")
+    saved_model = h2o.load_model(os.getcwd()+"/XGBoost_1_AutoML_20181105_211213")
     preds = saved_model.predict(h2oTest)
     preds = preds.as_data_frame()
     print(saved_model.model_performance(h2oTest))
     print("========================================================")
-    print("saving prediction into csv file")
+    print("Saving prediction into csv file")
     preds.to_csv("test_predict.csv")
+    return preds
   
+
+#Saved Residual plot
+def residual_plot(y_pred, y_test):
+    # transform y_test and y_pred as dataframe:
+    tf = y_test.to_frame().reset_index(drop=True)
+    new_df = pd.DataFrame()
+    new_df["y_test"] = tf
+    new_df["y_test_pred"] = y_pred
+    # substraction two columns to compute residuals:
+    new_df["sub"] = new_df[["y_test","y_test_pred"]].sum(axis=1)
+    yy = new_df[["y_test_pred","sub"]].sub(new_df["y_test"],axis=0)
+
+    # residual plot
+    plt.figure(figsize=(15,15))
+    plt.hlines(y=0, xmin=-5000, xmax=350000,color='black', lw=0.8)
+    sns.scatterplot(x = "sub", y= "y_test_pred", data = yy,color = "limegreen",marker = 's', edgecolor = "white",
+                   label = "Test Data")
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residuals')
+    plt.legend(loc='upper left')
+    plt.savefig('TEST_Residual_Xgboost.png', bbox_inches='tight',dpi=800)
+
+
 #saved_model.model_performance(h2oTest)  
 
+y_pred = h2o_pred(df)
+print("Prediction is done. test_predict.csv is saved in the current directory")
 
-h2o_pred(df)
-print("prediction is done. Saved in the current directory")
-print("")
-print("the csv file is called test_predict.csv")
+residual_plot(y_pred, df["Salary"])
+print("========================================================")
+print("Saved Residual plot into TEST_Residual_Xgboost.png in current directory")
+
+
+print("========================================================")
 print("done")
 
 
